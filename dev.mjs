c3d5fb490acf03a5a0d5fc29c49c49b8b525b686
Dario Piotrowicz <dario@cloudflare.com>
2024-02-25 23:06:24 +0000

    add dev script

---
+++ dev.mjs
@@ -0,0 +1,38 @@
+import { fork, execSync } from "node:child_process";
+import { join, resolve, dirname } from "node:path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+let wranglerDevResolve;
+const wranglerDevPromise = new Promise((r) => (wranglerDevResolve = r));
+
+const wranglerDevProcess = fork(
+  join(__dirname, "node_modules", "wrangler", "bin", "wrangler.js"),
+  ["dev", "--local", `--port=0`],
+  {
+    cwd: resolve(__dirname, "do-worker"),
+    env: { BROWSER: "none", ...process.env },
+    stdio: ["ignore", "ignore", "ignore", "ipc"],
+  }
+).on("message", () => {
+  wranglerDevResolve();
+});
+
+wranglerDevProcess.on("SIGINT", () => {
+  console.log("SIGINT, byebye");
+  wranglerDevProcess.exit();
+});
+
+wranglerDevProcess.on("SIGTERM", () => {
+  console.log("SIGTERM, byebye");
+  wranglerDevProcess.exit();
+});
+
+await wranglerDevPromise;
+
+execSync("npm run dev", {
+  cwd: resolve(__dirname, "sveltekit-app"),
+  stdio: "inherit",
+});
