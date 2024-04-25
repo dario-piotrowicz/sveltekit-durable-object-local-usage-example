8a3ab16425f91312725f066ae3c63a12d34e5586
ekhaled <me.khaled@gmail.com>
2024-04-25 19:40:24 +0100

    Share persistence folder This allows both `wrangler dev` sessions to share the same KV, D1 etc

--- dev.mjs
+++ dev.mjs
@@ -10,7 +10,7 @@ const wranglerDevPromise = new Promise((r) => (wranglerDevResolve = r));
 
 const wranglerDevProcess = fork(
   join(__dirname, "node_modules", "wrangler", "bin", "wrangler.js"),
-  ["dev"],
+  ["dev", "--persist-to", "../.wrangler"],
   {
     cwd: resolve(__dirname, "do-worker"),
     env: { BROWSER: "none", ...process.env },
